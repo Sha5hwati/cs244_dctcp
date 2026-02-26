c476df02a9e2fc0e0764ce00a0fc057117d691b2
Alisha Nanda <alishananda@google.com>
2026-02-25 21:20:38 -0800

    Add script to generate traffic

---
+++ generate_traffic.py
@@ -0,0 +1,39 @@
+import time
+import os
+from mininet.log import info
+
+def generate_traffic(net, traffic_pattern, sender_cca, log_directory, num_senders):
+    receiver = net.get('receiver')
+    receiver_ip = receiver.IP()
+    senders = []
+    for i in range(num_senders):
+        senders.append(net.get(f'sender{i+1}'))
+    
+    # iperf3 only supports flow from a single client to a single server port. To
+    # support flows from multiple senders, we have to open an iperf3 server on 
+    # a different port for each sender.
+    for port in range(5001, 5001 + num_senders):
+        receiver.cmd(f'sudo iperf3 -s -p {port} -D')
+    time.sleep(1)
+
+    info(f"Generating Traffic: {traffic_pattern}\n")
+
+    if traffic_pattern == 'elephant_vs_mice':
+        # We generate one long-lived (elephant) flow that will run for 15 seconds.
+        senders[0].cmd(f'sudo iperf3 -c {receiver_ip} -p 5001 -t 15 -C {sender_cca} -J --logfile {log_directory}/elephant.json &')
+        time.sleep(2)
+        # For each of the other senders, send 500KB of data on a short-lived (mouse) flow.
+        for i, s in enumerate(senders[1:]):
+            s.cmd(f'sudo iperf3 -c {receiver_ip} -p {5002+i} -n 500K -C {sender_cca} -J --logfile {log_directory}/mouse_{i}.json &')
+
+    elif traffic_pattern == 'constant':
+        # Each sender just sends a constant stream of 5 Mbps from each sender for 15 seconds. 
+        # TODO: adjust bitrate as needed if this creates too much congestion.
+        for i, s in enumerate(senders):
+            s.cmd(f'sudo iperf3 -c {receiver_ip} -p {5001+i} -t 15 -b 5M -C {sender_cca} -J --logfile {log_directory}/h{i+1}.json &')
+
+    elif traffic_pattern == 'bursty':
+        # Each sender has a target bitrate of 2 Mbps, sending in bursts of 20 packets for 15 seconds.
+        # TODO: adjust bitrate as needed if this creates too much congestion.
+        for i, s in enumerate(senders):
+            s.cmd(f'sudo iperf3 -c {receiver_ip} -p {5001+i} -t 15 -b 2M/20 -C {sender_cca} -J --logfile {log_directory}/h{i+1}.json &')
